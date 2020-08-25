82a061690e608565b055c50fbfa1f9f41285c7ee
Savio Fonseca <savifon1@gmail.com>
2020-08-25 22:53:02 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+/*
+Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+*/
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+  }
+}
